47a55cfa430b2afb4728b997aba37a3384b64de2
80883080@qq.com <80883080@qq.com>
2017-01-17 18:23:41 +0800

    缩放功能添加

--- js/index.js
+++ js/index.js
@@ -2,13 +2,16 @@ mui.init();
 window.onload = function(){
 	$CesiumContorller.init();
 }
-
 $CesiumContorller={};
+$CesiumContorller.Zoom=true;
 $CesiumContorller.init = function(){
     $CesiumContorller.map = new AMap.Map('container',{
             resizeEnable: true,
             zoom: 12,
             center: [116.480983, 39.989628]
+    });
+    AMap.event.addListener($CesiumContorller.map,'zoomend',function(){
+    	console.log($CesiumContorller.map.getZoom())
     });
     for (var i=0;i<10;i++) {
     	i%2 == 0?type = true:type=false;
@@ -19,7 +22,8 @@ $CesiumContorller.init = function(){
     function initIcon(center,type,id){
     	var clickDom = $CesiumContorller.addIcon(center,type,id);
 	    clickDom.on("click",function(){
-	   		console.log(this.G.name);
+	   		mui(".contentBox")[0].style.display="block";
+	   		mui(".info")[0].innerHTML=this.G.name;
 	    })
     }
    
@@ -27,13 +31,12 @@ $CesiumContorller.init = function(){
 $CesiumContorller.addIcon=function(center,type,id){
 	var color;
 	var marker = new AMap.Marker({
-		name:type+id,
+		name:type+"-"+id,
         position: center,
     });
     type ? color ="#09f" : color = "#ff0000"
     marker.setMap($CesiumContorller.map);
     var circle = new AMap.Circle({
-    		name:type+id,
             center: center,
             redius: 100,
             fillOpacity:0.1,
