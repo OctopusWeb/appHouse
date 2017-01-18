feeeeaf7ba62dc5f74b0b695b3d2ebcf6c358d94
80883080@qq.com <80883080@qq.com>
2017-01-18 17:53:20 +0800

    地图基础功能添加

--- js/index.js
+++ js/index.js
@@ -1,22 +1,31 @@
 mui.init();
 window.onload = function(){
 	$CesiumContorller.init();
+	mui(".call")[0].addEventListener("tap",function(){
+		plus.device.dial("114");
+	})
 }
 $CesiumContorller={};
 $CesiumContorller.Zoom=true;
+$CesiumContorller.markers=[];
 $CesiumContorller.init = function(){
     $CesiumContorller.map = new AMap.Map('container',{
             resizeEnable: true,
             zoom: 12,
             center: [116.480983, 39.989628]
     });
-    AMap.event.addListener($CesiumContorller.map,'zoomend',function(){
-    	console.log($CesiumContorller.map.getZoom())
-    });
     for (var i=0;i<10;i++) {
     	i%2 == 0?type = true:type=false;
     	var center = [116.480983+i*0.01, 39.989628+i*0.01];
     	initIcon(center,type,"a"+i)
+    }
+    addCluster();
+
+    // 添加点聚合
+    function addCluster(tag) {
+        $CesiumContorller.map.plugin(["AMap.MarkerClusterer"], function() {
+            cluster = new AMap.MarkerClusterer($CesiumContorller.map, $CesiumContorller.markers);
+        });
     }
     
     function initIcon(center,type,id){
@@ -24,26 +33,34 @@ $CesiumContorller.init = function(){
 	    clickDom.on("click",function(){
 	   		mui(".contentBox")[0].style.display="block";
 	   		mui(".info")[0].innerHTML=this.G.name;
+//	   		var bol = this.G.name.substr(0,this.G.name.indexOf("-"));
+//	   		if(bol == "true"){
+//	   			mui(".typeBtn")[0].style.display="block";
+//	   			mui(".typeBtn")[1].style.display="none";
+//	   		}else{
+//	   			mui(".typeBtn")[0].style.display="none";
+//	   			mui(".typeBtn")[1].style.display="block";
+//	   		}
+//	   		
 	    })
     }
    
 }
 $CesiumContorller.addIcon=function(center,type,id){
 	var color;
+	var url;
+	type ? url = "image/npt0.png" : url = "image/npt2.png"
 	var marker = new AMap.Marker({
 		name:type+"-"+id,
         position: center,
+        icon: new AMap.Icon({            
+            size: new AMap.Size(100, 100),  //图标大小
+            image: url,
+            imageOffset: new AMap.Pixel(0, 0)
+        })  
     });
     type ? color ="#09f" : color = "#ff0000"
     marker.setMap($CesiumContorller.map);
-    var circle = new AMap.Circle({
-            center: center,
-            redius: 100,
-            fillOpacity:0.1,
-            fillColor:color,
-            strokeColor:color,
-            strokeWeight:1
-    })
-    circle.setMap($CesiumContorller.map);
+    $CesiumContorller.markers.push(marker);
     return marker;
 }
